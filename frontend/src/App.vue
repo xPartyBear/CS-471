282706d0ea9056a0e99f7824f2059c79f019a834
Ethan <ethan.rowe512@gmail.com>
2024-04-23 13:52:15 -0400

    eliminating unneeded references

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import ping from '/services/ping.js'
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
 </script>
 
