86254487ca52d84fd4b4e025fae0f082eb1c9150
derppancakes <71749473+derppancakes@users.noreply.github.com>
2024-04-24 16:08:45 -0400

    Task/54 clear base template stuff (#8)
    * [#38] Generate Initial Fileshttps://openproject.michaeldekoski.com/work_packages/38
    
    * revert changes
    
    * [#54] Clear Base Template Stuffhttps://openproject.michaeldekoski.com/work_packages/54
    
    * error handled
    
    * removing old files
    
    * eliminating unneeded references
    
    ---------
    
    Co-authored-by: Michael Dekoski <michaeldekoski@gmail.com>

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,54 @@
+<script setup>
+import {RouterLink} from 'vue-router'
+import SideBar from './SideBar.vue'
+</script>
+
+<template>
+    <div>
+        <SideBar imgSrc="../../public/favicon.ico" title="Hello! Name HERE">
+            <div class="pageLink">
+                <RouterLink class="link" to="/">Home</RouterLink>
+                <RouterLink class="link" to="/past-puzzles">Past Puzzles</RouterLink>
+                <RouterLink class="link" to="/accounts">Accounts</RouterLink>
+                <button @click="">
+                    Sign Up / In
+                </button>
+            </div>
+        </SideBar>
+        Daily Pokedex
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NavBar',
+        data() {
+            return {
+
+            }
+        },  
+        methods: {
+            displaySignUp(){
+                this.$emit('sign-up');
+            },
+        }
+    }
+</script>
+
+<style>
+    header {
+        background-color: gray;
+        float: left;
+        position: fixed;
+        width: 100%;
+        margin: 0px;
+        height: 64px;
+        display: block;
+    }
+    .pageLink {
+        display: block;
+    }
+    .link {
+        display: block; 
+    }
+</style>
