083557f8ac6997cad61df266304b99418e6e393c
Ethan <ethan.rowe512@gmail.com>
2024-04-26 13:16:11 -0400

    Sign Up Modal Start

--- frontend/src/components/SignUpIn.vue
+++ frontend/src/components/SignUpIn.vue
@@ -1,13 +1,51 @@
 <template>
-    <div>
-        
+    <div class="modal">
+        <div @click="close()" class ="bg"></div>
+        <div class="window">
+            <h2 style="text-align: center;">Sign Up</h2>
+        </div>
     </div>
 </template>
 
 <script>
-
+    export default {
+        methods: {
+            close(){
+                this.$emit('close');
+            }
+        }
+    }
 </script>
 
 <style scoped>
+    .modal {
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        
+    }
+    .bg {
+        background-color: gray;
+        opacity: 50%;
+        position: fixed;
+        width: 100%;
+        height: 100%;
 
+        z-index: 5;
+
+    }
+    .window {
+        z-index: 6;
+        width: 35%;
+        height: 35%;
+        background-color: lightgray;
+        position: absolute;
+        left: 30%;
+        top: 30%;
+        border-radius: 16px;
+
+    }
+    .form {
+        display: block;
+    }
 </style>
