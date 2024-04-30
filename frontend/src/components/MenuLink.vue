67563c5c2fad833cabfe4aaa6ed022e23c704b52
Ethan <ethan.rowe512@gmail.com>
2024-04-30 13:57:00 -0400

    [#45] Pokemon Search Frontend  https://openproject.michaeldekoski.com/work_packages/45

--- frontend/src/components/MenuLink.vue
+++ frontend/src/components/MenuLink.vue
@@ -37,8 +37,6 @@
         width:100%;
         height:64px;
         float: right;
-        top: 35%;
-
     }
 
 </style>
---
+++ frontend/src/components/PokemonSearchElement.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <img class="icon" :src="iconSrc">
+        <div class="content"><slot></slot></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['pokemonImg'],
+        name: 'PokemonSearchElement',
+        computed: {
+            iconSrc(){
+                return this.pokemonImg != null ? this.pokemonImg : '../../public/favicon.ico';
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+    .container {
+        display: flex;
+        .icon {
+            width: 32px;
+            height: 32px;
+            
+        }
+        .content {
+            padding-left: 10%;
+        }
+    }
+</style>
